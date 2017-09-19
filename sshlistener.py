09f8d46b43698caecb60ed2dce00fd2bc2d3ee78
tobymohr <tobiasmohr22@gmail.com>
2017-09-19 14:07:10 +0200

    initial upload

---
+++ sshlistener.py
@@ -0,0 +1,166 @@
+import time
+import subprocess
+import select
+import thread
+import os
+from subprocess import call
+import shlex
+import glob
+import tarfile
+from os.path import basename
+
+
+ACCEPTED = "Accepted password"
+NEW_SESSION = "New session"
+REMOVED_SESSION = "Removed session"
+DISCONNECTED = "Disconnected from"
+NOT_FOUND = -1
+sessions = []
+path = r'/etc/woosh/packages/wooshserver/' 
+logpath = "/var/log/auth.log"
+logFile = path + "executelog.txt"
+
+
+def main():
+	resetAndListen()
+
+def resetAndListen():
+	#CLEAR SSH LOG
+	open(logpath, 'w').close()
+	createLogFile()
+
+	#TAIL SSH LOG
+	f = subprocess.Popen(['tail','-F',logpath],\
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	p = select.poll()
+	p.register(f.stdout)
+
+	#MAKE WOOSH DIR
+	makeDir(path)
+
+	while True:
+		if p.poll(1):
+			checkTail(f.stdout.readline())
+		time.sleep(0.5)
+
+
+'''-------------------METHODS FOR OPERATING ON FILES/FOLDERS-------------------'''
+
+def createLogFile():
+	if os.path.exists(logFile):
+		fh = open(logFile, "r")
+	else:
+		fh = open(logFile, "w")
+
+def appendToLog(filename):
+	with open(logFile,"a") as log:
+		log.write(filename + " " + str(os.path.getctime(filename)) + "\r\n")
+
+def isNewFile(filename):
+	isNew = True
+	if os.stat(logFile).st_size != 0:
+		with open(logFile) as file:
+			last_line = file.readlines()[-1]
+			linesplit = last_line.split(" ")
+			path = linesplit[0]
+			time = linesplit[1].split("\r")[0]
+			numberTime = float(time)
+			newTime = float(os.path.getctime(filename))
+			isNew = (int(newTime) > numberTime)
+	return isNew
+
+
+def make_executable(path):
+    mode = os.stat(path).st_mode
+    mode |= (mode & 0o444) >> 2
+    os.chmod(path, mode)
+
+def makeDir(inputPath):
+	if not os.path.exists(inputPath):
+		os.makedirs(inputPath)
+
+def findNewestArchive(path):
+	list_of_files = glob.glob(path +'/*.tar.gz') 
+	latest_file = max(list_of_files, key=os.path.getctime)
+	return latest_file
+
+def executeShellScriptFiles(folderpath):
+	filesForExecution = []
+	for file in os.listdir(folderpath):
+		if file.endswith(".sh"):
+			filesForExecution.append(os.path.join(folderpath,file))
+
+	for file in filesForExecution:
+		make_executable(file)
+		subprocess.call([file])
+
+def extractArchive(archive):
+	destination = basename(archive).split(".")[0]
+	newPath = path + destination
+	makeDir(newPath)
+	tar = tarfile.open(archive)
+	tar.extractall(path=newPath)
+	tar.close()
+	return newPath
+
+
+'''-------------------METHODS FOR CHECKING TAIL-------------------'''
+
+def checkTail(line):
+	words = None
+	beginWork = False
+	sessionId = 0
+	if contains(line, ACCEPTED):
+		words = ACCEPTED
+	elif contains(line, NEW_SESSION):
+		words = NEW_SESSION
+	elif contains(line, REMOVED_SESSION):
+		words = REMOVED_SESSION
+		beginWork = True
+
+	if words is not None:
+		index = indexOfWords(words, line)
+		sessionId = stripForId(index)
+		if words == NEW_SESSION:
+			sessions.append(sessionId)
+
+	if beginWork is True:
+		startThread(sessionId)
+
+def contains(line, word_to_find):
+	return word_to_find in line
+
+def stripForId(sentence):
+	newSentence = sentence.replace(".", "")
+	stuff =  [int(s) for s in newSentence.split() if s.isdigit()]
+	return stuff[0]
+
+def indexOfWords(words, sentence):
+	wordlist = words.split(" ")
+	maxWords = len(wordlist)
+	count = 0
+	for word in wordlist:
+		place = sentence.find(word)
+		if place != NOT_FOUND:
+			count+=1
+		if count == maxWords:
+			return sentence[place+len(word):]
+
+'''-------------------METHODS FOR THREADS-------------------'''
+
+
+def workerMethod(threadname, sessionId):
+	newestArchive = findNewestArchive(path)
+	if isNewFile(newestArchive):
+		pathToExtract  = extractArchive(newestArchive)
+		appendToLog(newestArchive)
+		executeShellScriptFiles(pathToExtract)
+	
+	sessions.remove(sessionId)
+
+def startThread(sessionId):
+	if sessionId in sessions:
+		thread.start_new_thread(workerMethod, ("Thread executed by session  " + str(sessionId), sessionId ))
+
+main()
+
